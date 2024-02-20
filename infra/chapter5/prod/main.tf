ca6c93100b92b25517dca0f2209d635a4042fdee
skunikan <skunikan@amazon.co.jp>
2024-02-21 02:39:31 +0900

    fix

--- infra/chapter5/prod/main.tf
+++ infra/chapter5/prod/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "~> 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
